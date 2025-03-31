fe329b6ab4fc2a7b577dd71fec337dc08b9c54c7
Scared <nathan@vittoni.ch>
2025-03-31 19:13:49 +0200

    [INIT] intialisation page login

---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function login(){
+    return(
+        <main>
+            <form>
+            <label>Enter your name:
+                <input type="text" />
+            </label>
+            <label>Enter your email
+                    <input type="text" />
+            </label>
+            
+            </form>
+        </main>
+    );
+};
+
+export default login;
