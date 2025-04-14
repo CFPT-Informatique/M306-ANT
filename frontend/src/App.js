a4fe93a05a7f766ce01f756de828dc17fec2c619
SCARED <vittoninathan0@gmail.com>
2025-04-14 11:33:15 +0200

    login 100% fonctionnel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
@@ -25,15 +26,23 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<PrivateRoute loggedIn={loggedIn} element={<Home />} />} />
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
-          <Route path="/register" element={<PrivateRoute loggedIn={loggedIn} element={<Register />} />} />
-          <Route path="/home" element={<PrivateRoute loggedIn={loggedIn} element={<Home />} />} />
-          {/* <Route path="/" element={loggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />} />
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} /> */}
+      <Routes>
+          <Route 
+            path="/" 
+            element={<PrivateRoute loggedIn={loggedIn} element={<Home />} />} 
+          />
+          <Route 
+            path="/login" 
+            element={<PublicRoute loggedIn={loggedIn} element={<Login setLoggedIn={setLoggedIn} />} />} 
+          />
+          <Route 
+            path="/register" 
+            element={<PublicRoute loggedIn={loggedIn} element={<Register />} />} 
+          />
+          <Route 
+            path="/home" 
+            element={<PrivateRoute loggedIn={loggedIn} element={<Home />} />} 
+          />
         </Routes>
       </Router>
     </div>
